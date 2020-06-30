a033520ace6ad6a479a079fa8f05e85e486224c2
y-w-y-contact <47558916+y-w-y-contact@users.noreply.github.com>
2020-06-30 22:52:57 +0800

    0

--- extension/d/js/index.js
+++ extension/d/js/index.js
@@ -13,20 +13,72 @@ function ywy_detect_file_parameter() {
         resolve(this_search);
     });
 }
+
+function ywy_quality_to_text(this_quality) {
+    return new Promise(function (resolve, reject) {
+        let this_quality_text = "";
+        switch (this_quality) {
+            case "120":
+                this_quality_text = "4K";
+                break;
+            case "116":
+                this_quality_text = "1080P60";
+                break;
+            case "112":
+                this_quality_text = "1080P+";
+                break;
+            case "80":
+                this_quality_text = "1080P";
+                break;
+            case "74":
+                this_quality_text = "720P60";
+                break;
+            case "64":
+                this_quality_text = "720P";
+                break;
+            case "32":
+                this_quality_text = "480P";
+                break;
+            case "16":
+                this_quality_text = "360P";
+                break;
+
+            default:
+                this_quality_text = "自動最高畫質";
+                break;
+        }
+        resolve(this_quality_text);
+    });
+}
 /*函數庫結束*/
 
 async function ywy_console() {
     let ywy_has_file_parameter = await ywy_detect_file_parameter();
-    if(!ywy_has_file_parameter){
+    if (!ywy_has_file_parameter) {
         alert("無法獲取檔案訊息，請透過正確的方式開啟下載網頁。");
-    }else{
+    } else {
         let ywy_file_parameter = new URL(window.location.href).searchParams.get("file");
         let ywy_file_string = ywy_base64_decode(ywy_file_parameter);
         let ywy_file_json = JSON.parse(ywy_file_string);
-        
-        document.getElementById("ywy_image_box").src = ywy_file_json.picture;
+        if (ywy_file_json.type == "bangumi") {
+
+        } else if (ywy_file_json.type == "video") {
+            //填入基本訊息開始//
+            document.getElementById("ywy_image_box").src = ywy_file_json.picture;
+            document.getElementById("ywy_media_title_mother").innerText = `名稱: ${ywy_file_json.title_mother}`;
+            document.getElementById("ywy_media_title_child").innerText = `集數: ${ywy_file_json.title_child}`;
+            document.getElementById("ywy_media_quality").innerText = `畫質: ${await ywy_quality_to_text(ywy_file_json.quality)} (若影片經過後製，可能會判斷不準確)`;
+            document.getElementById("ywy_media_url").innerText = `原始網址: ${ywy_file_json.url}`;
+            document.getElementById("ywy_media_picture").innerText = `封面圖片: ${ywy_file_json.picture}`;
+            let ywy_file_size_sum = 0;
+            console.log(ywy_file_json)
+            //填入基本訊息結束//
+
+        } else if (ywy_file_json.type == "audio") {
+
+        }
     }
-}  
+}
 
 /*開關開始*/
 ywy_console();
