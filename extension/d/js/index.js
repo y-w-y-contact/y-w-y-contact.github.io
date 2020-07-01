16b358e6c541ac6557d7b10e69cac2f1476cebb9
y-w-y-contact <47558916+y-w-y-contact@users.noreply.github.com>
2020-07-02 01:58:25 +0800

    Update index.js

--- extension/d/js/index.js
+++ extension/d/js/index.js
@@ -83,7 +83,7 @@ function ywy_xhr(this_url) {
         let xhr = new XMLHttpRequest();
         xhr.addEventListener("readystatechange", function (e) {
             if (xhr.readyState == 4) {
-                let this_blob = URL.createObjectURL(xhr.response);
+                let this_blob = xhr.response;
                 ywy_g_files.push(this_blob);
                 resolve("done")
             }
@@ -137,17 +137,12 @@ async function ywy_download(ywy_file_json) {
     if (ywy_g_files.length > 1) {
         document.getElementById("ywy_button_download_video").innerText = "合併下載分段";
 
-        let this_flvs_file = [];
-        for (let i = 0; i < ywy_g_files.length; i++) {
-            this_flvs_file.push(new File([ywy_g_files[i]], `ywy_flv_file_${i}`, { type: "video/x-flv", lastModified: Date.now() }));
-        }
-
         //建立flv集開始//
         let this_flvs = [];
         for (let i = 0; i < ywy_g_files.length; i++) {
             this_flvs.push({
-                name: this_flvs_file[i].name,
-                file: this_flvs_file[i]
+                name: `this_flvs_file_${i}`,
+                file: ywy_g_files[i]
             });
         }
         console.log(this_flvs)
