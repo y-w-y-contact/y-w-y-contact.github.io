3b5e70614b0a09fe6c4cdb05640ebadea67a78e5
y-w-y-contact <47558916+y-w-y-contact@users.noreply.github.com>
2023-06-28 17:13:31 +0800

    0

--- test2/merge.js
+++ test2/merge.js
@@ -1,33 +1,44 @@
-function mergeFiles() {
-    // Get the file objects from the input fields.
-    const audioFile = document.getElementById('audioFile').files[0];
-    const videoFile = document.getElementById('videoFile').files[0];
-  
-    // Merge the files.
-    mergeM4AandM4S(audioFile, videoFile, 'output.mp4');
+// Load the M4A audio and M4S video files
+const audioURL = 'audio.m4a';
+const videoURL = 'video.m4s';
+
+const videoElement = document.createElement('video');
+const mediaSource = new MediaSource();
+
+videoElement.src = URL.createObjectURL(mediaSource);
+
+mediaSource.addEventListener('sourceopen', handleSourceOpen);
+
+function handleSourceOpen() {
+  const sourceBuffer = mediaSource.addSourceBuffer('video/mp4; codecs="avc1.42E01E"');
+
+  fetch(audioURL)
+    .then(response => response.arrayBuffer())
+    .then(audioData => {
+      // Append the audio data to the source buffer
+      sourceBuffer.appendBuffer(audioData);
+    });
+
+  fetch(videoURL)
+    .then(response => response.arrayBuffer())
+    .then(videoData => {
+      // Append the video data to the source buffer
+      sourceBuffer.appendBuffer(videoData);
+    });
+
+  sourceBuffer.addEventListener('updateend', handleUpdateEnd);
+}
+
+function handleUpdateEnd() {
+  if (!mediaSource.updating && mediaSource.readyState === 'open') {
+    // The media source has finished updating, create the URL for the merged video
+    const mergedURL = URL.createObjectURL(mediaSource);
+
+    // Set the merged video URL as the source for a video element
+    const mergedVideoElement = document.createElement('video');
+    mergedVideoElement.src = mergedURL;
+
+    // Append the video element to the DOM or perform any desired actions
+    document.body.appendChild(mergedVideoElement);
   }
-  
-  // Merge the M4A audio file and the M4S video file into a single MP4 file.
-  function mergeM4AandM4S(audioFile, videoFile, outputFile) {
-    // Create a new MediaSource object.
-    const mediaSource = new MediaSource();
-  
-    // Add the M4A audio file to the MediaSource object.
-    mediaSource.addSourceBuffer(new ArrayBuffer(audioFile));
-  
-    // Add the M4S video file to the MediaSource object.
-    mediaSource.addSourceBuffer(new ArrayBuffer(videoFile));
-  
-    // Create a new MediaStream object.
-    const mediaStream = new MediaStream();
-  
-    // Add the MediaSource object to the MediaStream object.
-    mediaStream.addTrack(mediaSource.getTrackById('audio'));
-    mediaStream.addTrack(mediaSource.getTrackById('video'));
-  
-    // Create a new Blob object from the MediaStream object.
-    const blob = new Blob([mediaStream], {type: 'video/mp4'});
-  
-    // Save the Blob object to a file.
-    saveAs(blob, outputFile);
-  }
+}
